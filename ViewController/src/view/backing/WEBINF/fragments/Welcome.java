d29482e7a66d3de36502b17d73739677bbb28712
Ajay Krishnan <ajajkris@oradev.oraclecorp.com>
2017-08-18 17:59:23 +0530

    The freaking pattern

---
+++ ViewController/src/view/backing/WEBINF/fragments/Welcome.java
@@ -0,0 +1,18 @@
+package view.backing.WEBINF.fragments;
+
+import oracle.adf.view.rich.component.rich.output.RichActiveOutputText;
+
+
+public class Welcome {
+
+
+    private RichActiveOutputText aot1;
+
+    public void setAot1(RichActiveOutputText aot1) {
+        this.aot1 = aot1;
+    }
+
+    public RichActiveOutputText getAot1() {
+        return aot1;
+    }
+}
