7734d5a0418534c5cfa5069e0c0abd5dd11c8c2b
Akhil Raj <akhraj@oradev.oraclecorp.com>
2017-08-18 15:11:50 +0530

    init

---
+++ ViewController/src/view/backing/pages/Admin.java
@@ -0,0 +1,123 @@
+package view.backing.pages;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.fragment.RichPageTemplate;
+import oracle.adf.view.rich.component.rich.layout.RichDecorativeBox;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
+import oracle.adf.view.rich.component.rich.layout.RichPanelList;
+import oracle.adf.view.rich.component.rich.nav.RichCommandNavigationItem;
+import oracle.adf.view.rich.component.rich.nav.RichLink;
+import oracle.adf.view.rich.component.rich.nav.RichNavigationPane;
+
+public class Admin {
+    private RichPageTemplate pt1;
+    private RichForm f1;
+    private RichDocument d1;
+    private RichNavigationPane np1;
+    private RichCommandNavigationItem ni1;
+    private RichCommandNavigationItem ni2;
+    private RichDecorativeBox db1;
+    private RichPanelGroupLayout pgl1;
+    private RichPanelList pl1;
+    private RichLink l1;
+    private RichLink l2;
+    private RichLink l3;
+
+    public void setPt1(RichPageTemplate pt1) {
+        this.pt1 = pt1;
+    }
+
+    public RichPageTemplate getPt1() {
+        return pt1;
+    }
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+
+    public void setNp1(RichNavigationPane np1) {
+        this.np1 = np1;
+    }
+
+    public RichNavigationPane getNp1() {
+        return np1;
+    }
+
+    public void setNi1(RichCommandNavigationItem ni1) {
+        this.ni1 = ni1;
+    }
+
+    public RichCommandNavigationItem getNi1() {
+        return ni1;
+    }
+
+    public void setNi2(RichCommandNavigationItem ni2) {
+        this.ni2 = ni2;
+    }
+
+    public RichCommandNavigationItem getNi2() {
+        return ni2;
+    }
+
+    public void setDb1(RichDecorativeBox db1) {
+        this.db1 = db1;
+    }
+
+    public RichDecorativeBox getDb1() {
+        return db1;
+    }
+
+    public void setPgl1(RichPanelGroupLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGroupLayout getPgl1() {
+        return pgl1;
+    }
+
+
+    public void setPl1(RichPanelList pl1) {
+        this.pl1 = pl1;
+    }
+
+    public RichPanelList getPl1() {
+        return pl1;
+    }
+
+    public void setL1(RichLink l1) {
+        this.l1 = l1;
+    }
+
+    public RichLink getL1() {
+        return l1;
+    }
+
+    public void setL2(RichLink l2) {
+        this.l2 = l2;
+    }
+
+    public RichLink getL2() {
+        return l2;
+    }
+
+    public void setL3(RichLink l3) {
+        this.l3 = l3;
+    }
+
+    public RichLink getL3() {
+        return l3;
+    }
+}
