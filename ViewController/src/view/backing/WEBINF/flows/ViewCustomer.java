13b7278ef942a8da7c5e77d908b18eb85d62e089
Akhil Raj <akhraj@oradev.oraclecorp.com>
2017-08-20 15:10:37 +0530

    working on page

---
+++ ViewController/src/view/backing/WEBINF/flows/ViewCustomer.java
@@ -0,0 +1,262 @@
+package view.backing.WEBINF.flows;
+
+
+import oracle.adf.view.rich.component.rich.layout.RichDecorativeBox;
+import oracle.adf.view.rich.component.rich.layout.RichGridCell;
+import oracle.adf.view.rich.component.rich.layout.RichGridRow;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGridLayout;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
+import oracle.adf.view.rich.component.rich.layout.RichPanelTabbed;
+import oracle.adf.view.rich.component.rich.layout.RichShowDetailItem;
+import oracle.adf.view.rich.component.rich.nav.RichLink;
+import oracle.adf.view.rich.component.rich.output.RichImage;
+import oracle.adf.view.rich.component.rich.output.RichOutputText;
+import oracle.adf.view.rich.component.rich.output.RichSpacer;
+
+
+public class ViewCustomer {
+
+
+    private RichPanelGridLayout pgl1;
+    private RichGridRow gr1;
+    private RichGridCell gc1;
+    private RichGridRow gr2;
+    private RichGridCell gc2;
+    private RichGridCell gc3;
+    private RichImage i1;
+    private RichSpacer s1;
+    private RichOutputText ot1;
+    private RichOutputText ot2;
+    private RichPanelGroupLayout pgl2;
+    private RichSpacer s2;
+    private RichLink l1;
+    private RichSpacer s3;
+    private RichGridRow gr3;
+    private RichGridCell gc4;
+    private RichSpacer s4;
+    private RichGridCell gc5;
+    private RichDecorativeBox db1;
+    private RichGridRow gr4;
+    private RichGridCell gc6;
+    private RichPanelTabbed pt1;
+    private RichShowDetailItem tab1;
+    private RichShowDetailItem tab2;
+    private RichShowDetailItem tab3;
+    private RichShowDetailItem tab4;
+
+    public void setPgl1(RichPanelGridLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGridLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setGr1(RichGridRow gr1) {
+        this.gr1 = gr1;
+    }
+
+    public RichGridRow getGr1() {
+        return gr1;
+    }
+
+    public void setGc1(RichGridCell gc1) {
+        this.gc1 = gc1;
+    }
+
+    public RichGridCell getGc1() {
+        return gc1;
+    }
+
+    public void setGr2(RichGridRow gr2) {
+        this.gr2 = gr2;
+    }
+
+    public RichGridRow getGr2() {
+        return gr2;
+    }
+
+    public void setGc2(RichGridCell gc2) {
+        this.gc2 = gc2;
+    }
+
+    public RichGridCell getGc2() {
+        return gc2;
+    }
+
+    public void setGc3(RichGridCell gc3) {
+        this.gc3 = gc3;
+    }
+
+    public RichGridCell getGc3() {
+        return gc3;
+    }
+
+
+    public void setI1(RichImage i1) {
+        this.i1 = i1;
+    }
+
+    public RichImage getI1() {
+        return i1;
+    }
+
+
+    public void setS1(RichSpacer s1) {
+        this.s1 = s1;
+    }
+
+    public RichSpacer getS1() {
+        return s1;
+    }
+
+
+    public void setOt1(RichOutputText ot1) {
+        this.ot1 = ot1;
+    }
+
+    public RichOutputText getOt1() {
+        return ot1;
+    }
+
+    public void setOt2(RichOutputText ot2) {
+        this.ot2 = ot2;
+    }
+
+    public RichOutputText getOt2() {
+        return ot2;
+    }
+
+    public void setPgl2(RichPanelGroupLayout pgl2) {
+        this.pgl2 = pgl2;
+    }
+
+    public RichPanelGroupLayout getPgl2() {
+        return pgl2;
+    }
+
+    public void setS2(RichSpacer s2) {
+        this.s2 = s2;
+    }
+
+    public RichSpacer getS2() {
+        return s2;
+    }
+
+    public void setL1(RichLink l1) {
+        this.l1 = l1;
+    }
+
+    public RichLink getL1() {
+        return l1;
+    }
+
+    public void setS3(RichSpacer s3) {
+        this.s3 = s3;
+    }
+
+    public RichSpacer getS3() {
+        return s3;
+    }
+
+
+    public void setGr3(RichGridRow gr3) {
+        this.gr3 = gr3;
+    }
+
+    public RichGridRow getGr3() {
+        return gr3;
+    }
+
+    public void setGc4(RichGridCell gc4) {
+        this.gc4 = gc4;
+    }
+
+    public RichGridCell getGc4() {
+        return gc4;
+    }
+
+
+    public void setS4(RichSpacer s4) {
+        this.s4 = s4;
+    }
+
+    public RichSpacer getS4() {
+        return s4;
+    }
+
+
+    public void setGc5(RichGridCell gc5) {
+        this.gc5 = gc5;
+    }
+
+    public RichGridCell getGc5() {
+        return gc5;
+    }
+
+
+    public void setDb1(RichDecorativeBox db1) {
+        this.db1 = db1;
+    }
+
+    public RichDecorativeBox getDb1() {
+        return db1;
+    }
+
+    public void setGr4(RichGridRow gr4) {
+        this.gr4 = gr4;
+    }
+
+    public RichGridRow getGr4() {
+        return gr4;
+    }
+
+    public void setGc6(RichGridCell gc6) {
+        this.gc6 = gc6;
+    }
+
+    public RichGridCell getGc6() {
+        return gc6;
+    }
+
+
+    public void setPt1(RichPanelTabbed pt1) {
+        this.pt1 = pt1;
+    }
+
+    public RichPanelTabbed getPt1() {
+        return pt1;
+    }
+
+    public void setTab1(RichShowDetailItem tab1) {
+        this.tab1 = tab1;
+    }
+
+    public RichShowDetailItem getTab1() {
+        return tab1;
+    }
+
+    public void setTab2(RichShowDetailItem tab2) {
+        this.tab2 = tab2;
+    }
+
+    public RichShowDetailItem getTab2() {
+        return tab2;
+    }
+
+    public void setTab3(RichShowDetailItem tab3) {
+        this.tab3 = tab3;
+    }
+
+    public RichShowDetailItem getTab3() {
+        return tab3;
+    }
+
+    public void setTab4(RichShowDetailItem tab4) {
+        this.tab4 = tab4;
+    }
+
+    public RichShowDetailItem getTab4() {
+        return tab4;
+    }
+}
---
+++ ViewController/src/view/backing/WEBINF/fragments/CustomerOverview.java
@@ -0,0 +1,133 @@
+package view.backing.WEBINF.fragments;
+
+import oracle.adf.view.rich.component.rich.RichQuickQuery;
+import oracle.adf.view.rich.component.rich.data.RichTable;
+import oracle.adf.view.rich.component.rich.layout.RichDecorativeBox;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
+import oracle.adf.view.rich.component.rich.layout.RichToolbar;
+import oracle.adf.view.rich.component.rich.nav.RichButton;
+import oracle.adf.view.rich.component.rich.nav.RichCommandLink;
+import oracle.adf.view.rich.component.rich.output.RichSpacer;
+
+public class CustomerOverview {
+    private RichPanelGroupLayout pgl1;
+    private RichDecorativeBox db1;
+    private RichToolbar t1;
+    private RichButton b1;
+    private RichButton b2;
+    private RichButton b3;
+    private RichPanelGroupLayout pgl2;
+    private RichQuickQuery qryId1;
+    private RichCommandLink cl1;
+    private RichTable t2;
+    private RichSpacer s1;
+    private RichSpacer s2;
+    private RichSpacer s3;
+
+    public void setPgl1(RichPanelGroupLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGroupLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setDb1(RichDecorativeBox db1) {
+        this.db1 = db1;
+    }
+
+    public RichDecorativeBox getDb1() {
+        return db1;
+    }
+
+    public void setT1(RichToolbar t1) {
+        this.t1 = t1;
+    }
+
+    public RichToolbar getT1() {
+        return t1;
+    }
+
+    public void setB1(RichButton b1) {
+        this.b1 = b1;
+    }
+
+    public RichButton getB1() {
+        return b1;
+    }
+
+    public void setB2(RichButton b2) {
+        this.b2 = b2;
+    }
+
+    public RichButton getB2() {
+        return b2;
+    }
+
+    public void setB3(RichButton b3) {
+        this.b3 = b3;
+    }
+
+    public RichButton getB3() {
+        return b3;
+    }
+
+    public void setPgl2(RichPanelGroupLayout pgl2) {
+        this.pgl2 = pgl2;
+    }
+
+    public RichPanelGroupLayout getPgl2() {
+        return pgl2;
+    }
+
+
+    public void setQryId1(RichQuickQuery qryId1) {
+        this.qryId1 = qryId1;
+    }
+
+    public RichQuickQuery getQryId1() {
+        return qryId1;
+    }
+
+    public void setCl1(RichCommandLink cl1) {
+        this.cl1 = cl1;
+    }
+
+    public RichCommandLink getCl1() {
+        return cl1;
+    }
+
+    public void setT2(RichTable t2) {
+        this.t2 = t2;
+    }
+
+    public RichTable getT2() {
+        return t2;
+    }
+
+    public void setS1(RichSpacer s1) {
+        this.s1 = s1;
+    }
+
+    public RichSpacer getS1() {
+        return s1;
+    }
+
+    public void setS2(RichSpacer s2) {
+        this.s2 = s2;
+    }
+
+    public RichSpacer getS2() {
+        return s2;
+    }
+
+    public void setS3(RichSpacer s3) {
+        this.s3 = s3;
+    }
+
+    public RichSpacer getS3() {
+        return s3;
+    }
+
+
+}
