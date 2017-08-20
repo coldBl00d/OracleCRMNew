3fc975a03e7b8bb590120b374e20033b60dfb750
akhraj <akhraj@AKHRAJ-LAP.oradev.oraclecorp.com>
2017-08-20 13:37:59 +0530

    working view commit

--- ViewController/src/view/backing/pages/Launcher.java
+++ ViewController/src/view/backing/pages/Launcher.java
@@ -8,6 +8,21 @@ import oracle.ui.pattern.dynamicShell.TabContext;
 
 public class Launcher {
     
+    
+    public void manageCustomerActivity(ActionEvent actionEvent) 
+      { 
+        /** 
+        * Example method when called repeatedly, will open another instance as 
+        * oppose to selecting a previously opened tab instance. Note the boolean 
+        * to create another tab instance is set to true. 
+        */ 
+          
+        _launchActivity( 
+          "Roles", 
+          "/WEB-INF/flows/manage-customer-taskflow.xml#manage-customer-taskflow",  
+          true); 
+      } 
+    
     /* dont edit this */
     public void manageRoleActivity(ActionEvent actionEvent) 
       { 
