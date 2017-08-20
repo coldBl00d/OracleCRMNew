50b56693ec892c9dc1af1576768334409242640d
akhraj <akhraj@AKHRAJ-LAP.oradev.oraclecorp.com>
2017-08-20 08:48:42 +0530

    finsih adf security

--- ViewController/src/view/backing/pages/Login.java
+++ ViewController/src/view/backing/pages/Login.java
@@ -2,10 +2,23 @@ package view.backing.pages;
 
 import oracle.adf.view.rich.component.rich.RichDocument;
 import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.input.RichInputText;
+import oracle.adf.view.rich.component.rich.layout.RichGridCell;
+import oracle.adf.view.rich.component.rich.layout.RichGridRow;
+import oracle.adf.view.rich.component.rich.layout.RichPanelBox;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGridLayout;
+import oracle.adf.view.rich.component.rich.nav.RichButton;
 
 public class Login {
     private RichForm f1;
     private RichDocument d1;
+    private RichPanelGridLayout pgl1;
+    private RichGridRow gr1;
+    private RichGridCell gc1;
+    private RichPanelBox pb1;
+    private RichInputText it1;
+    private RichInputText it2;
+    private RichButton b1;
 
     public void setF1(RichForm f1) {
         this.f1 = f1;
@@ -21,5 +34,61 @@ public class Login {
 
     public RichDocument getD1() {
         return d1;
+    }
+
+    public void setPgl1(RichPanelGridLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGridLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setGr1(RichGridRow gr1) {
+        this.gr1 = gr1;
+    }
+
+    public RichGridRow getGr1() {
+        return gr1;
+    }
+
+    public void setGc1(RichGridCell gc1) {
+        this.gc1 = gc1;
+    }
+
+    public RichGridCell getGc1() {
+        return gc1;
+    }
+
+    public void setPb1(RichPanelBox pb1) {
+        this.pb1 = pb1;
+    }
+
+    public RichPanelBox getPb1() {
+        return pb1;
+    }
+
+    public void setIt1(RichInputText it1) {
+        this.it1 = it1;
+    }
+
+    public RichInputText getIt1() {
+        return it1;
+    }
+
+    public void setIt2(RichInputText it2) {
+        this.it2 = it2;
+    }
+
+    public RichInputText getIt2() {
+        return it2;
+    }
+
+    public void setB1(RichButton b1) {
+        this.b1 = b1;
+    }
+
+    public RichButton getB1() {
+        return b1;
     }
 }
